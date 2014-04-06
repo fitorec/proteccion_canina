e534a431c888a9030e62ae7786c98a265d35e5d2
Fitorec <chanerec@gmail.com>
2014-04-06 00:13:31 -0600

    Agregando archivos necesarios para el diseño base

--- web/app/Config/core.php
+++ web/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '#$$EDSGFFSDGHDFHDE%&&()(?=(');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '432814165784501021463871475135576632');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
